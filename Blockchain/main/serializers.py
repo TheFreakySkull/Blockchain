42948520c16b02454ccf5ae0d2fe735fd654f61c
Artem <koyushkovartem@gmail.com>
2025-09-21 23:42:42 +0300

    Transaction validation system done + bit of refactoring

--- Blockchain/main/serializers.py
+++ Blockchain/main/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers, exceptions, status
 from .models import Transaction, Utxo
+from django.db.model import Count
 import hashlib
+import ecdsa
 
 class OutputSerializer(ModelSerializer):
     class Meta:
         model = Utxo
-        fields = ['recepient_pubkey', 'sender_pubkey', 'amount']
+        fields = ['id','recepient_pubkey', 'sender_pubkey', 'amount']
         read_only_fields = ['sender_pubkey']
 
 class InputSerializer(ModelSerializer):
@@ -24,25 +26,45 @@ class TransactionSerializer(ModelSerializer):
         fields = ['inputs', 'outputs', 'sender_pubkey', 'recepient_pubkey', 'signature']
 
     def create(self, validated_data):
-        inputs = validated_data['inputs'].filter(spent=False)
-        outputs = validated_data['outputs']
+        inputs = validated_data['inputs'].filter(spent=False, 
+                                                 recepient_pubkey=validated_data['sender_pubkey'])\
+                                         .aggregate(inputs_amount=Count('amount'))
+        outputs = validated_data['outputs'].aggregate(outputs_amount=Count('amount'))
         if inputs is None:
-            raise exceptions.ValidationError({'inputs': 'inputs were already spent'}, code=status.HTTP_402_PAYMENT_REQUIRED)
-        amount = 0
-        for input in inputs:
-            if input.recepient_pubkey == validated_data['sender_pubkey']:
-                amount += input.amount
-                continue
-            raise exceptions.ValidationError({'input': 'dosent belong to sender'}, code=status.HTTP_403_FORBIDDEN)
-        map(lambda x: amount - x.amount, outputs)
-        if amount < 0:
-            raise exceptions.ValidationError({'outputs': 'outputs amount more than inputs amount'}, code=status.HTTP_402_PAYMENT_REQUIRED)
+            raise exceptions.ValidationError({'inputs': 'inputs were already spent or does\'nt'\
+                                              'belong to sender'}, code=status.HTTP_402_PAYMENT_REQUIRED)
+
+        if inputs.inputs_amount - outputs.outputs_amount < 0:
+            raise exceptions.ValidationError({'outputs': 'outputs amount more than inputs amount'},
+                                             code=status.HTTP_402_PAYMENT_REQUIRED)
+
         transaction_recipe = f'Inputs:{inputs.values('id')}, '\
                         f'Outputs:{outputs.values('recepient_pubkey, amount')}'\
                         f'Sender_pubkey: {validated_data['sender_pubkey']}'
 
         hash = hashlib.sha256(transaction_recipe).hexdigest()
+        sender_pubkey_bytes = bytes.fromhex(validated_data['sender_pubkey'])
+        vk = ecdsa.VerifyingKey.from_string(sender_pubkey_bytes, curve=ecdsa.SECP256k1,
+                                            hashfunc=hashlib.sha256)
+        try:
+            vk.verify(validated_data['signature'],hash)
+        except ecdsa.BadSignatureError:
+            raise exceptions.ValidationError({'signature': 'signature is not valid'},
+                                             code=status.HTTP_403_FORBIDDEN)
+
+        transaction = Transaction.objects.create(**validated_data, hash=hash)
+        for input in validated_data['inputs']:
+            utxo = Utxo.objects.get(input).update(spent=True)
+            transaction.inputs.add(utxo)
+
+        for output in validated_data['outputs']:
+            utxo = Utxo.objects.create(**output,
+                                sender_pubkey=validated_data['sender_pubkey'])
+            transaction.outputs.add(utxo)
 
         
         
-        
+
+
+
+
